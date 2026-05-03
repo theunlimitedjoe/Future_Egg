798cdeb44ac46c2e206a7b68c4ade6df5c0ae54a
Jo <theunlimitedmailbox@gmail.com>
2026-05-03 13:27:50 -0400

    Keyboard Removal

--- addons/xogot_connect/debug_monitor.gd
+++
@@ -1,7 +0,0 @@
-extends EditorPlugin
-
-# func _notification(what):
-# 	if what == EditorPlugin.NOTIFICATION_EDITOR_PLAY:
-# 		print("Debug session started.")
-# 	elif what == EditorPlugin.NOTIFICATION_EDITOR_STOP:
-# 		print("Debug session stopped.")
--- addons/xogot_connect/xogot_debug.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-# Single debug flag for the entire Xogot Remote Debugger addon
-# Set to false for release builds to suppress all debug output
-const ENABLED = false
